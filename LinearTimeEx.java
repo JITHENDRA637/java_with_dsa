828d1b829b37aa859eaa5336089fc8c90579885a
JITHENDRA637 <jithendrareddyreddem@gmail.com>
2025-06-11 14:05:00 +0530

    Add files via upload

---
+++ LinearTimeEx.java
@@ -0,0 +1,13 @@
+package java_with_dsa;
+
+public class LinearTimeEx {
+	public static   void printAllElements(int [] arr) {
+		for (int i= 0; i < arr.length; i++) {
+			System.out.println("Element " + i + ": " +arr[i]);
+		}
+	}
+	public static void main(String[] args) {
+		int[] numbers= {10,20,30,40,50};
+		printAllElements(numbers);
+	}
+}
