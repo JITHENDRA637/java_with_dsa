ba4ac211134dcb0426e464be73ab10c1c5fb5cd3
JITHENDRA637 <jithendrareddyreddem@gmail.com>
2025-06-11 14:44:47 +0530

    Add files via upload

---
+++ ExponetialTimeEx.java
@@ -0,0 +1,14 @@
+package java_with_dsa;
+
+public class ExponetialTimeEx {
+	public static int fibonacci(int n) {
+		if (n <= 1) {
+			return n;
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println("Fibonacci of " + n +" is: " +fibonacci(n));
+	}
+}
